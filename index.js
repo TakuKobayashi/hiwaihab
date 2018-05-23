1231e81665c50cf60de286bdd46303f1b29223a3
TakuKobayshi <>
2018-05-23 17:25:51 +0900

    処理をスリムにしてlinebotとしてお返事できるようにつくった

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var AWS = require('aws-sdk');
-var dynamo = new AWS.DynamoDB({
-    region: 'ap-northeast-1'
-});
+//var AWS = require('aws-sdk');
+//var dynamo = new AWS.DynamoDB({
+//    region: 'ap-northeast-1'
+//});
 
 var line = require('@line/bot-sdk');
-var crypto = require('crypto');
-var client = new line.Client({channelAccessToken: process.env.ACCESSTOKEN});
 
 var Pornsearch = require('pornsearch');
 
@@ -15,34 +13,17 @@ var search = function(keyword, callback){
 };
 
 exports.handler = function (event, context) {
-  var signature = crypto.createHmac('sha256', process.env.CHANNELSECRET).update(event.body).digest('base64');
-  var checkHeader = (event.headers || {})['X-Line-Signature'];
-  var body = JSON.parse(event.body);
-  if (signature === checkHeader) {
-    if (body.events[0].replyToken === '00000000000000000000000000000000') { //接続確認エラー回避
-      var lambdaResponse = {
-        statusCode: 200,
-        headers: { "X-Line-Status": "OK"},
-        body: '{"result":"connect check"}'
-      };
-      context.succeed(lambdaResponse);
-    } else {
-      var text = body.events[0].message.text;
-      var message = {
-        'type': 'text',
-        'text': text
-      };
-      client.replyMessage(body.events[0].replyToken, message)
-      .then((response) => {
-        var lambdaResponse = {
-          statusCode: 200,
-          headers: { "X-Line-Status" : "OK"},
-          body: '{"result":"completed"}'
-        };
-        context.succeed(lambdaResponse);
-      }).catch((err) => console.log(err));
-    }
-  }else{
-    console.log('署名認証エラー');
-  }
+  var client = new line.Client({channelAccessToken: process.env.ACCESSTOKEN});
+  var message = {
+    type: "text",
+    text: JSON.stringify(event)
+  };
+  client.replyMessage(event.events[0].replyToken, message).then((response) => {
+    var lambdaResponse = {
+      statusCode: 200,
+      headers: { "X-Line-Status" : "OK"},
+      body: '{"result":"completed"}'
+    };
+    context.succeed(lambdaResponse);
+  }).catch((err) => console.log(err));
 };
